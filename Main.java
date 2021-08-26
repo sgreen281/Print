5593c93e780f2027f0ad6aec547d02e5c7be5c51
sgreen281 <89562361+sgreen281@users.noreply.github.com>
2021-08-26 18:27:33 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,41 @@
+class Main {
+  public static void main(String[] args) {
+// Snake 
+    System.out.println("\t \t \t     |");
+    System.out.println("\t \t \t     |");
+    System.out.println("\t \t \t     |");
+    System.out.println("\t \t \t     o");
+    System.out.println("\t \t \t    o o");
+    System.out.println("\t \t \t    o o");
+    System.out.println("\t \t \t    ooo");
+    System.out.println("\t \t \t    ooo");
+    System.out.println("\t \t \t   ooo");    
+    System.out.println("\t \t \t  ooo");    
+    System.out.println("\t \t \t  ooo");
+    System.out.println("\t \t \t   ooo");
+    System.out.println("\t \t \t     ooo");
+    System.out.println("\t \t \t       ooo");
+    System.out.println("\t \t \t       ooo");
+    System.out.println("\t \t \t      ooo");
+    System.out.println("\t \t \t     ooo");
+    System.out.println("\t \t \t    ooo");
+    System.out.println("\t \t \t   ooo");
+    System.out.println("\t \t \t  ooo");
+    System.out.println("\t \t \t  ooo");
+    System.out.println("\t \t \t   ooo");
+    System.out.println("\t \t \t    ooo");
+    System.out.println("\t \t \t     ooo");
+    System.out.println("\t \t \t      ooo");
+    System.out.println("\t \t \t       ooo");
+    System.out.println("\t \t \t       ooo");
+    System.out.println("\t \t \t      ooo");
+    System.out.println("\t \t \t     ooo");
+    System.out.println("\t \t \t    ooo");
+    System.out.println("\t \t \t   ooo");
+    System.out.println("\t \t \t  ooo");
+    System.out.println("\t \t \t  ooo");
+    System.out.println("\t \t \t   ooo");
+    System.out.println("\t \t \t    ooo");
+    System.out.println("\t \t \t     ooo");
+  }
+}
